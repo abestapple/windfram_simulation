8b3fe528154e16f70b8b814df56617518bf92e3b
abestapple <35458450+abestapple@users.noreply.github.com>
2024-10-24 16:28:11 +0800

    Add files via upload

---
+++ login.py
@@ -0,0 +1,40 @@
+import hmac  
+import streamlit as st  
+def check_password(): 
+    def login_form(): 
+        with st.form("Credentials"):  
+            col0,col1, col2 = st.columns([0.6,0.4, 2],vertical_alignment="top")  
+            with col0:  
+                st.write("  ")  
+            with col1:  
+                #st.image("./utils/logo.png",  width=75)  
+                st.write("  ")
+            with col2:  
+                st.markdown("## WINDFARM ANALYSIS")  
+            st.text_input("Username", key="username")  
+            st.text_input("Password", type="password", key="password")  
+            # 将按钮靠右放置  
+            cols = st.columns([4, 1])  
+            cols[1].form_submit_button("Login", on_click=password_entered)  
+            # st.form_submit_button("登录", on_click=password_entered)   
+    def password_entered():  
+        """Checks whether a password entered by the user is correct."""  
+        if st.session_state["username"] in st.secrets[  
+            "passwords"  
+        ] and hmac.compare_digest(  
+            st.session_state["password"],  
+            st.secrets.passwords[st.session_state["username"]],  
+        ):  
+            st.session_state["password_correct"] = True  
+            del st.session_state["password"]  
+            del st.session_state["username"]  
+        else:  
+            st.session_state["password_correct"] = False  
+    if st.session_state.get("password_correct", False):  
+        # st.success("登录成功！")  
+        return True  
+    # Show inputs for username + password.  
+    login_form()  
+    if "password_correct" in st.session_state:  
+        st.error("😕 账号不存在或者密码不正确")  
+    return False   
